ffb7b1cd2d4beca6e8871f5c7c3edf3d1224ac6e
eseodiase <50231281+eseodiase@users.noreply.github.com>
2022-08-17 11:41:45 +0100

    header.js

--- components/header.js
+++ components/header.js
@@ -1,1 +1,11 @@
-
+const header =()=>{
+                return `<div class="header">
+    <label class="myName">Ese Odiase</label>
+    
+    <div class="darkmode">
+    <span class="darkmodeBtn"><span class="roundToggle"></span>
+    </span>
+    </div> 
+                     </div>`;
+};
+export default header;
