c8fb464ba898109cd8f7f542138740dddfe83018
eseodiase <50231281+eseodiase@users.noreply.github.com>
2022-08-17 21:05:22 +0100

    iframe2.js

---
+++ nftComponents/iframe2.js
@@ -0,0 +1,8 @@
+//
+            const iframe2 =()=>{
+                return`<div class="iframe">
+                <div class="card">image3</div>
+                </div>`;
+            }
+            
+            export default iframe2;
