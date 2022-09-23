dc5915557747f2b0be5e13bc08dcb244938e0848
eseodiase <50231281+eseodiase@users.noreply.github.com>
2022-09-23 15:42:35 +0100

    Add files via upload

---
+++ projects/images/nft_images.js
@@ -0,0 +1,94 @@
+const allNFTs =()=>{
+            const iframe0 =()=>{
+                return`<div class="iframe">
+                <div class="card">
+                <a href="https://eseodiase.github.io/SinglePageJSApp/" target="_blank">
+                <img src="https://eseodiase.github.io/new-ese-portfolio/projects/images/jsapp.jpg" class="nfts" alt="JS App Theme"/>
+                </a>
+                </div>
+                <p>
+                Simple Theme
+                <br/>
+                <span class="languages">
+                HTML/CSS/JavaScript
+                </span>
+                </p>
+                </div>`;
+            }
+
+            //
+            const iframe1 =()=>{
+                return`<div class="iframe">
+                <div class="card">
+                <a href="https://eseodiase.github.io/NourishMe/" target="_blank">
+                <img src="https://eseodiase.github.io/new-ese-portfolio/projects/images/nourishme.jpg" class="nfts" alt="NourishMe Theme"/>
+                </a>
+                </div>
+                <p>
+                E-commerce Theme
+                <br/>
+                <span class="languages">
+                HTML/CSS/JavaScript
+                </span>
+                </p>
+                </div>`;
+            }
+            
+            //
+            const iframe2 =()=>{
+                return`<div class="iframe">
+                <div class="card">
+                <a href="https://eseodiase.github.io/Motif/" target="_blank">
+                <img src="https://eseodiase.github.io/new-ese-portfolio/projects/images/motif.jpg" class="nfts" alt="Motif Theme"/>
+                </a>
+                </div>
+                <p>
+                Portfolio Theme
+                <br/>
+                <span class="languages">
+                HTML/CSS/JavaScript
+                </span>
+                </p>
+                </div>`;
+            }
+            
+            const iframe3 =()=>{
+                return`<div class="iframe">
+                <div class="card">
+                <a href="https://eseodiase.github.io/Brainie/" target="_blank">
+                <img src="https://eseodiase.github.io/new-ese-portfolio/projects/images/Brainie.jpg" class="nfts" alt="Brainie Theme"/>
+                </a>
+                </div>
+                <p>
+                Publisher Template
+                <br/>
+                <span class="languages">
+                HTML/CSS/JavaScript
+                </span>
+                </p>
+                </div>`;
+            }
+            
+const iframe4 =()=>{
+                return`<div class="iframe">
+                <div class="card">
+                
+                <img src="https://eseodiase.github.io/new-ese-portfolio/projects/images/coming.png" class="nfts" alt="Coming Soon"/>
+                
+                </div>
+                <p>
+                Web App
+                <br/>
+                <span class="languages">
+                React JS
+                </span>
+                </p>
+                </div>`;
+}
+            
+            const allFrames =()=>{
+              document.querySelector(".myIframe").innerHTML = ( iframe0 () + iframe1 () + iframe2() + iframe3() + iframe4());
+            }
+            allFrames();
+}
+export default allNFTs;
