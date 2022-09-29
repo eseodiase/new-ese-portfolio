fff524b88fa6e8e1315fc5a409753459e932c110
Ese Odiase <50231281+eseodiase@users.noreply.github.com>
2022-09-29 15:14:18 +0100

    home.js

--- components/home.js
+++ components/home.js
@@ -75,12 +75,17 @@ return `<section class="homePage">
     </div>
     
     <div class="box3">
+    <a href="twitter.com/shapemytech">
     <i class="fa fa-twitter"></i>
+    </a>
+
     <i class="fa fa-instagram"></i>
     <i class="fa fa-youtube"></i>
+
     <a href="wa.me/2348153929729">
     <i class="fa fa-whatsapp"></i>
     </a>
+
     <i class="fa fa-github"></i>
     </div>
                  
