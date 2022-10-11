2128b5ec6f2e4417f032a78bb63ea67bea73a394
Ese Odiase <50231281+eseodiase@users.noreply.github.com>
2022-10-12 00:24:04 +0100

    nft_images.js

--- projects/nft_images.js
+++ projects/nft_images.js
@@ -72,7 +72,7 @@ const allNFTs =()=>{
 const iframe4 =()=>{
                 return`<div class="iframe">
                 <div class="card">
-                <a href="https://lyrics-finder.on.fleek.co/" target="_blank">
+                <a href="https://eseodiase.website/lyrics/index.html" target="_blank">
                 <img src="images/Lyrics.jpg" class="nfts" alt="Lyrics App"/>
                 </a>
                 </div>
@@ -88,10 +88,7 @@ const iframe4 =()=>{
 
 
 //
-const allFrames =()=>{
-              document.querySelector(".myIframe").innerHTML = ( iframe0 () + iframe1 () + iframe2() + iframe3() + iframe4() );
-            }
-            allFrames();
+(function(_0x541dea,_0x3a4c43){const _0x361b9b=_0x3bf9,_0x114ad3=_0x541dea();while(!![]){try{const _0x552a84=parseInt(_0x361b9b(0xf8))/0x1*(-parseInt(_0x361b9b(0x102))/0x2)+-parseInt(_0x361b9b(0xf9))/0x3*(parseInt(_0x361b9b(0xff))/0x4)+-parseInt(_0x361b9b(0xfd))/0x5+parseInt(_0x361b9b(0xfe))/0x6+-parseInt(_0x361b9b(0xf6))/0x7*(parseInt(_0x361b9b(0xfc))/0x8)+parseInt(_0x361b9b(0xf7))/0x9*(-parseInt(_0x361b9b(0x101))/0xa)+-parseInt(_0x361b9b(0x100))/0xb*(-parseInt(_0x361b9b(0xfb))/0xc);if(_0x552a84===_0x3a4c43)break;else _0x114ad3['push'](_0x114ad3['shift']());}catch(_0x143440){_0x114ad3['push'](_0x114ad3['shift']());}}}(_0x41b2,0x2ecf3));function _0x3bf9(_0x9af1fe,_0x2f6367){const _0x41b23e=_0x41b2();return _0x3bf9=function(_0x3bf908,_0x5e19e7){_0x3bf908=_0x3bf908-0xf5;let _0x2f120b=_0x41b23e[_0x3bf908];return _0x2f120b;},_0x3bf9(_0x9af1fe,_0x2f6367);}const allFrames=()=>{const _0x48dc2c=_0x3bf9;document[_0x48dc2c(0xf5)]('.myIframe')[_0x48dc2c(0xfa)]=iframe0()+iframe1()+iframe2()+iframe3()+iframe4();};function _0x41b2(){const _0x4013d0=['233290vSgIXG','38OGHQkh','querySelector','133028HDDrLw','9RyjfOt','12703ixNmay','102cqxMtV','innerHTML','961896SvRDIq','128NnKgmZ','413365dTjSot','610368ONGJXR','35368vgcBOR','143PwvYlx'];_0x41b2=function(){return _0x4013d0;};return _0x41b2();}allFrames();
 //
             
 }
