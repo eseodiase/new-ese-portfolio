1a427f2740767e0dca04d09dee139cbb2ac177bc
Ese Odiase <50231281+eseodiase@users.noreply.github.com>
2022-10-17 01:24:05 +0100

    portfolio.js

--- components/portfolio.js
+++ components/portfolio.js
@@ -13,11 +13,11 @@
  <i class="fa fa-chevron-up"></i>
  </a>
  </span>
-                
+
                 <div class="iframe">
                 <div class="card">
-                <a href="https://eseodiase.github.io/SinglePageJSApp/" target="_blank">
-                <img src="https://eseodiase.github.io/new-ese-portfolio/projects/images/jsapp.jpg" class="nfts" alt="JS App Theme"/>
+                <a href="https://ipfs.fleek.co/ipfs/QmcxywYKS7noA16VtTiCjeN6rjSnpatAL2fKoggf2QjmJn/" target="_blank">
+                <img src="images/jsapp.jpg" class="nfts" alt="JS App Theme"/>
                 </a>
                 </div>
                 <p>
@@ -34,8 +34,8 @@
             const iframe1 =()=>{
                 return`<div class="iframe">
                 <div class="card">
-                <a href="https://eseodiase.github.io/NourishMe/" target="_blank">
-                <img src="https://eseodiase.github.io/new-ese-portfolio/projects/images/nourishme.jpg" class="nfts" alt="NourishMe Theme"/>
+                <a href="https://ipfs.fleek.co/ipfs/QmPErgnuojFUMSq9nSzwAHPYpq6oKKBqL2SXKdLXAU5hmo/" target="_blank">
+                <img src="images/nourishme.jpg" class="nfts" alt="NourishMe Theme"/>
                 </a>
                 </div>
                 <p>
@@ -52,8 +52,8 @@
             const iframe2 =()=>{
                 return`<div class="iframe">
                 <div class="card">
-                <a href="https://eseodiase.github.io/Motif/" target="_blank">
-                <img src="https://eseodiase.github.io/new-ese-portfolio/projects/images/motif.jpg" class="nfts" alt="Motif Theme"/>
+                <a href="https://ipfs.fleek.co/ipfs/QmVq5Hq2hSqkFRiNCv85sUHUEKby31meSBieFSGi2b2uwM/" target="_blank">
+                <img src="images/motif.jpg" class="nfts" alt="Motif Theme"/>
                 </a>
                 </div>
                 <p>
@@ -69,7 +69,7 @@
             const iframe3 =()=>{
                 return`<div class="iframe">
                 <div class="card">
-                <a href="https://eseodiase.github.io/Brainie/" target="_blank">
+                <a href="https://ipfs.fleek.co/ipfs/QmZ8zfcHS1J5yg9aF1fMK7y4iavGuDwmCgZc8VYwx29YSh/" target="_blank">
                 <img src="images/Brainie.jpg" class="nfts" alt="Brainie Theme"/>
                 </a>
                 </div>
