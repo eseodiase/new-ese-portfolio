79f194e4d941e9b10cb2a5c9d2595e41067a0a56
Ese Odiase <50231281+eseodiase@users.noreply.github.com>
2022-10-20 00:14:14 +0100

    Add files via upload

--- code_components/components/categories/subNav.js
+++ code_components/components/categories/subNav.js
@@ -1,1 +1,12 @@
-
+const subNav =()=>{
+    return `<section class="subNav">
+    <div class="sub">
+    <p class="ht"> HTML Code</p>
+    </div>
+    
+    <div class="sub">
+    <p class="pal"> Symbols </p>
+    </div>
+    </section>`;
+}
+export default subNav;
